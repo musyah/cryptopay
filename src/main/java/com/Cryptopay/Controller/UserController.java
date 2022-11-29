d99c5f74eb1cff4e0a6055f2b80227cc7165eb79
musyah <musyamichael3@gmail.com>
2022-11-29 16:59:17 +0300

    cryptopay

--- src/main/java/com/Cryptopay/Controller/UserController.java
+++ src/main/java/com/Cryptopay/Controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RestController
 @AllArgsConstructor
 @RequestMapping("/CryptoApp/Onboard")
